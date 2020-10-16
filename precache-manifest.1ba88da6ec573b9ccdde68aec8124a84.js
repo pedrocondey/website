08be5676b5eefe63bed901c23cec075494c10e6e
pedrocondey <63548180+pedrocondey@users.noreply.github.com>
2020-10-16 09:21:41 +0100

    Updates

---
+++ precache-manifest.1ba88da6ec573b9ccdde68aec8124a84.js
@@ -0,0 +1,218 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99408e259972c9e947f8b8e09933a251",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "20d6d950347696d737d3",
+    "url": "/website/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "27769c29e00b33cff26d",
+    "url": "/website/static/css/main.d6dc5a28.chunk.css"
+  },
+  {
+    "revision": "20d6d950347696d737d3",
+    "url": "/website/static/js/2.8e9dc619.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/website/static/js/2.8e9dc619.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27769c29e00b33cff26d",
+    "url": "/website/static/js/main.ac3979aa.chunk.js"
+  },
+  {
+    "revision": "040125e22865be079845",
+    "url": "/website/static/js/runtime-main.c566a718.js"
+  },
+  {
+    "revision": "4318a6f7bd42cb549e72e6740350fd3c",
+    "url": "/website/static/media/Shadow1.4318a6f7.jpg"
+  },
+  {
+    "revision": "1d0b9fe754dab13094e16b29fc8f02f3",
+    "url": "/website/static/media/Shadow10.1d0b9fe7.jpg"
+  },
+  {
+    "revision": "32dbefe2a3d01b5f3e77427c2dfd2345",
+    "url": "/website/static/media/Shadow2.32dbefe2.jpg"
+  },
+  {
+    "revision": "57fd12f86e82131452559733ab44f2cf",
+    "url": "/website/static/media/Shadow3.57fd12f8.jpg"
+  },
+  {
+    "revision": "193d3ae3b52de96cf1b5159d00e85379",
+    "url": "/website/static/media/Shadow4.193d3ae3.jpg"
+  },
+  {
+    "revision": "09d64528d5d8753f442e8a1d92b0d93b",
+    "url": "/website/static/media/Shadow5.09d64528.jpg"
+  },
+  {
+    "revision": "575e00cab1232f67592ead903b62c501",
+    "url": "/website/static/media/Shadow6.575e00ca.jpg"
+  },
+  {
+    "revision": "9172db42b0675e0028cd923584d381d2",
+    "url": "/website/static/media/Shadow7.9172db42.jpg"
+  },
+  {
+    "revision": "368146dcc97077115b76ee922abf090d",
+    "url": "/website/static/media/Shadow8.368146dc.jpg"
+  },
+  {
+    "revision": "755605c0bed5180eeda6ba66b77182d5",
+    "url": "/website/static/media/Shadow9.755605c0.jpg"
+  },
+  {
+    "revision": "1045062cfd91aa446f35a1f858891fe4",
+    "url": "/website/static/media/informis1.1045062c.jpg"
+  },
+  {
+    "revision": "1a217ab440e9d9c43f95d5eff134a8ef",
+    "url": "/website/static/media/informis10.1a217ab4.jpg"
+  },
+  {
+    "revision": "8290891e6095e9187f7c59ef06f11e54",
+    "url": "/website/static/media/informis2.8290891e.jpg"
+  },
+  {
+    "revision": "6af4521215362ae8997ca06bbe68ceb0",
+    "url": "/website/static/media/informis3.6af45212.jpg"
+  },
+  {
+    "revision": "0f3a12844e4ea6a55e97a41407d2a75b",
+    "url": "/website/static/media/informis4.0f3a1284.jpg"
+  },
+  {
+    "revision": "668138957f7d0886c3fe2a0dfed2e5b6",
+    "url": "/website/static/media/informis5.66813895.jpg"
+  },
+  {
+    "revision": "d0e3222faf9207593a038f45cad3fded",
+    "url": "/website/static/media/informis6.d0e3222f.jpg"
+  },
+  {
+    "revision": "d693180129fa67bf29d7e61aea90791e",
+    "url": "/website/static/media/informis7.d6931801.jpg"
+  },
+  {
+    "revision": "942c942e60a54c0cab03d11b69fa2a0d",
+    "url": "/website/static/media/informis8.942c942e.jpg"
+  },
+  {
+    "revision": "d861c539d7c1eb1b0c2d2ee2757dc999",
+    "url": "/website/static/media/informis9.d861c539.jpg"
+  },
+  {
+    "revision": "94a5c1619886a04484b98142359010ae",
+    "url": "/website/static/media/logo-about.94a5c161.png"
+  },
+  {
+    "revision": "8adf2272050f85d54dd61241ae4324d2",
+    "url": "/website/static/media/maria meira 01.8adf2272.jpg"
+  },
+  {
+    "revision": "1882b937d8ee11d8745ad1dd4ed91493",
+    "url": "/website/static/media/maria meira 02.1882b937.jpg"
+  },
+  {
+    "revision": "a82b303b27c85ee30e42b4ab1f7cf53b",
+    "url": "/website/static/media/maria meira 03.a82b303b.jpg"
+  },
+  {
+    "revision": "69bd3417273e38dbb9f170bf192fe3fa",
+    "url": "/website/static/media/maria meira 04.69bd3417.jpg"
+  },
+  {
+    "revision": "1fcdc2e3329b7ec0fe311e9b43d79e69",
+    "url": "/website/static/media/maria meira 05.1fcdc2e3.jpg"
+  },
+  {
+    "revision": "46fa1414ee65beb78a50309b8d72d461",
+    "url": "/website/static/media/maria meira 06.46fa1414.jpg"
+  },
+  {
+    "revision": "6f8b69eb71dbdb3a8378c01b99188afd",
+    "url": "/website/static/media/muto1.6f8b69eb.jpg"
+  },
+  {
+    "revision": "3b3286a022531e71017132c86a2b60cf",
+    "url": "/website/static/media/muto10.3b3286a0.jpg"
+  },
+  {
+    "revision": "35a69e8f840df25658da1fe56ce5a23f",
+    "url": "/website/static/media/muto2.35a69e8f.jpg"
+  },
+  {
+    "revision": "8d73a4d8f0020ff4abc810e8446e994a",
+    "url": "/website/static/media/muto3.8d73a4d8.jpg"
+  },
+  {
+    "revision": "059cea2022210cea332a53dbcc4281bd",
+    "url": "/website/static/media/muto4.059cea20.jpg"
+  },
+  {
+    "revision": "35d756fd5a4459fded0eba430a434ccb",
+    "url": "/website/static/media/muto5.35d756fd.jpg"
+  },
+  {
+    "revision": "b2ed3fc6b82de75ed7c2a646e2abb4e9",
+    "url": "/website/static/media/muto6.b2ed3fc6.jpg"
+  },
+  {
+    "revision": "1cabf76d3a2babd99421d8f86bc54400",
+    "url": "/website/static/media/muto7.1cabf76d.jpg"
+  },
+  {
+    "revision": "612a2a87dae0d665e3271aeedac7bcf1",
+    "url": "/website/static/media/muto8.612a2a87.jpg"
+  },
+  {
+    "revision": "434c6be3f789003fab004896b66d12d0",
+    "url": "/website/static/media/muto9.434c6be3.jpg"
+  },
+  {
+    "revision": "e30664d6c1a5c89007e506559988570a",
+    "url": "/website/static/media/voice1.e30664d6.jpg"
+  },
+  {
+    "revision": "dbb18341ff285ee1d2c66038427ef076",
+    "url": "/website/static/media/voice10.dbb18341.jpg"
+  },
+  {
+    "revision": "3ba0f5b8f3291b0b2d3ae1766e3bdac2",
+    "url": "/website/static/media/voice2.3ba0f5b8.jpg"
+  },
+  {
+    "revision": "aad18a6a63b7460b51de3435e524d0d6",
+    "url": "/website/static/media/voice3.aad18a6a.jpg"
+  },
+  {
+    "revision": "23d8c8fd99a7826f7510a8c2fcc21172",
+    "url": "/website/static/media/voice4.23d8c8fd.jpg"
+  },
+  {
+    "revision": "f2e05c49c8273d3f44f2d1b3f0854a0f",
+    "url": "/website/static/media/voice5.f2e05c49.jpg"
+  },
+  {
+    "revision": "3d027eb0adfe422ba64a5a7708bad740",
+    "url": "/website/static/media/voice6.3d027eb0.jpg"
+  },
+  {
+    "revision": "d7478bd5a3d609c2773a85fa66408d1f",
+    "url": "/website/static/media/voice7.d7478bd5.jpg"
+  },
+  {
+    "revision": "26122bd71d441fc8aca13fc2b5f04818",
+    "url": "/website/static/media/voice8.26122bd7.jpg"
+  },
+  {
+    "revision": "ef64d1194c9fb4985c06fcadeccb33e3",
+    "url": "/website/static/media/voice9.ef64d119.jpg"
+  }
+]);
